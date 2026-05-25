e02a868c059b036454bb4080f4bd28afcb4b1d07
snxwisalive <maks.roskoshenko@gmail.com>
2026-05-25 22:41:04 +0300

    add: design to product search

--- frontend/src/components/product/ProductSearch.jsx
+++ frontend/src/components/product/ProductSearch.jsx
@@ -1,34 +1,50 @@
 import { useNavigate } from "react-router-dom";
+import { BounceLoader } from "react-spinners";
+import styles from "../../styles/ProductSearch.module.css"
 
-export default function ProductSearch({query, onQueryChange, results, onSearch, mealType }) {
+export default function ProductSearch({query, onQueryChange, results, onSearch, mealType, isSearching }) {
     const navigate = useNavigate();
 
     return (
-        <div className="searchForm">
-            <div className="formGroup">
-                <input
-                    id="query"
-                    name="query"
-                    type="text"
-                    placeholder="Chocolate Milka"
-                    value={query}
-                    onChange={onQueryChange}
-                />
-                <button className="searchButton" onClick={onSearch}>Search</button>
-                <button className="addCustom" onClick={() => navigate('/products/create', {state: { mealType }})}>Add Custom Product</button>
-                <ul className="productsList">
-                    <div className="listGroup">
-                        {results.map((item) => (
-                            <li key={item.id} onClick={() => navigate(`/products/${item.externalId}`, { state: { mealType, product: item } })}>
-                                {item.productName} -  
-                                {item.calories} kcal - 
-                                {item.protein}g- 
-                                {item.carbs}g - 
-                                {item.fat}g
-                            </li>
-                        ))}
-                    </div>
-                </ul>
+        <div className={styles.searchForm}>
+            <div className={styles.formGroup}>
+                <div className={styles.searchRow}>
+                    <input
+                        id="query"
+                        className={styles.input}
+                        name="query"
+                        type="text"
+                        placeholder="Chocolate Milka"
+                        value={query}
+                        onChange={onQueryChange}
+                    />
+                    <button className={styles.searchButton} onClick={onSearch}>Search</button>
+                </div>
+
+                    <button className={styles.addCustom} onClick={() => navigate('/products/create', {state: { mealType }})}>
+                        Add Custom Product
+                    </button>
+                {isSearching ? (
+                    <>
+                        <div className={styles.loading}><BounceLoader color="var(--color-btn-bg)" size={60} speedMultiplier={1.2} /></div>
+                    </>
+                ) : (
+                    <ul className={styles.productsList}>
+                            {results.map((item) => (
+                                <li key={item.id} className={styles.productItem} onClick={() => navigate(`/products/${item.externalId}`, { state: { mealType, product: item } })}>
+                                    <div className={styles.productInfo}>
+                                        <span className={styles.productName}>{item.productName}</span>
+                                    </div>
+                                    <div className={styles.productMacros}>
+                                        <span>{item.calories} kcal</span>
+                                        <span>{item.protein} g</span>
+                                        <span>{item.carbs} g</span>
+                                        <span>{item.fat} g</span>
+                                    </div>
+                                </li>
+                            ))}
+                    </ul>
+                )}
             </div>
         </div>
     )
--- frontend/src/pages/ProductSearchPage.jsx
+++ frontend/src/pages/ProductSearchPage.jsx
@@ -6,17 +6,25 @@ import ProductSearch from "../components/product/ProductSearch";
 export default function ProductSearchPage() {
     const [query, setQuery] = useState("");
     const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
         
     const location = useLocation();
 
     const mealType = location.state?.mealType; 
 
     const fetchProducts = async () => {
-        const data = await fetchApi(`/api/products/search?query=${query}`)
-    
-        setResults(data)
+        setIsLoading(true);
 
-        console.log(data)
+        try {
+            const data = await fetchApi(`/api/products/search?query=${query}`)
+        
+            setResults(data)
+        } catch (error) {
+            setError("Service unavailable")
+        } finally {
+            setIsLoading(false);
+        }
     };
     
     const handleInputChange = (event) => {
@@ -31,6 +39,7 @@ export default function ProductSearchPage() {
                 results={results}
                 onSearch={fetchProducts}
                 mealType={mealType}
+                isSearching={isLoading}
             />
         </div>
     )
